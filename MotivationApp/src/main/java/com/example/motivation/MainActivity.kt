eb96ffd96f8fc724c27b7207692470da2c0cde72
Wagner Mello <wagnermellow2k@gmail.com>
2023-04-11 08:01:24 -0300

    motivation: added view binding and a click button as test

--- MotivationApp/src/main/java/com/example/motivation/MainActivity.kt
+++ MotivationApp/src/main/java/com/example/motivation/MainActivity.kt
@@ -2,12 +2,26 @@ package com.example.motivation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.motivation.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonNewPhrase.setOnClickListener(this)
 
         supportActionBar?.hide()
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.buttonNewPhrase) {
+            val s = ""
+        }
     }
 }
